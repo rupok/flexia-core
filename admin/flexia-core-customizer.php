b4e94eff6a8651a8600414009531114bcd411edb
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2018-01-15 17:53:10 +0600

    custom script backward compatibility added

--- admin/flexia-core-customizer.php
+++ admin/flexia-core-customizer.php
@@ -10,56 +10,52 @@
 
 function flexia_core_customize_register( $wp_customize ) {
 
-  // Header Script section
+  if( class_exists( 'WP_Customize_Code_Editor_Control') ) {
+    // Header Script section
+    $wp_customize->add_section( 'flexia_core_header_script_section' , array(
+    'title'      => __('Header Script','custom-script-for-customizer'), 
+    'priority'   => 10    
+    ) );  
 
-	$wp_customize->add_section( 'flexia_core_header_script_section' , array(
-	'title'      => __('Header Script','custom-script-for-customizer'), 
-	'priority'   => 10    
-	) );  
+    $wp_customize->add_setting( 'flexia_core_header_script' );
 
+      $control = new WP_Customize_Code_Editor_Control( $wp_customize, 'flexia_core_header_script', array(
+              'label'          => __( 'Add your header script without script tag', 'custom-script-for-customizer' ),
+              'section' => 'flexia_core_header_script_section',
+              'code_type' => 'text/javascript',
+              'settings'       => 'flexia_core_header_script',
+      ) );
+      $wp_customize->add_control( $control );
 
-	$wp_customize->add_setting( 'flexia_core_header_script' );
+    // Footer Script section
 
-    $control = new WP_Customize_Code_Editor_Control( $wp_customize, 'flexia_core_header_script', array(
-            'label'          => __( 'Add your header script without script tag', 'custom-script-for-customizer' ),
-            'section' => 'flexia_core_header_script_section',
-            'code_type' => 'text/javascript',
-            'settings'       => 'flexia_core_header_script',
+    $wp_customize->add_section( 'flexia_core_footer_script_section' , array(
+    'title'      => __('Footer Script','custom-script-for-customizer'), 
+    'priority'   => 20    
+    ) );  
+
+    $wp_customize->add_setting( 'flexia_core_footer_script' );
+      $control = new WP_Customize_Code_Editor_Control( $wp_customize, 'flexia_core_footer_script', array(
+              'label'          => __( 'Add your footer script without script tag', 'custom-script-for-customizer' ),
+              'section' => 'flexia_core_footer_script_section',
+              'code_type' => 'text/javascript',
+              'settings'       => 'flexia_core_footer_script',
+      ) );
+      $wp_customize->add_control( $control );
+
+    // Create custom panels
+    $wp_customize->add_panel( 'flexia_core_custom_scripts', array(
+        'priority' => 999,
+        'theme_supports' => '',
+        'title' => __( 'Custom JavaScripts', 'custom-script-for-customizer' ),
+        'description' => __( 'Add csutom scripts to header or footer', 'custom-script-for-customizer' ),
     ) );
-    $wp_customize->add_control( $control );
 
-  // Footer Script section
+    // Assign sections to panels
+    $wp_customize->get_section('flexia_core_header_script_section')->panel = 'flexia_core_custom_scripts';      
+    $wp_customize->get_section('flexia_core_footer_script_section')->panel = 'flexia_core_custom_scripts';
 
-	$wp_customize->add_section( 'flexia_core_footer_script_section' , array(
-	'title'      => __('Footer Script','custom-script-for-customizer'), 
-	'priority'   => 20    
-	) );  
-
-	$wp_customize->add_setting( 'flexia_core_footer_script' );
-    $control = new WP_Customize_Code_Editor_Control( $wp_customize, 'flexia_core_footer_script', array(
-            'label'          => __( 'Add your footer script without script tag', 'custom-script-for-customizer' ),
-            'section' => 'flexia_core_footer_script_section',
-            'code_type' => 'text/javascript',
-            'settings'       => 'flexia_core_footer_script',
-    ) );
-    $wp_customize->add_control( $control );
-
-
-
-  // Create custom panels
-  $wp_customize->add_panel( 'flexia_core_custom_scripts', array(
-      'priority' => 999,
-      'theme_supports' => '',
-      'title' => __( 'Custom JavaScripts', 'custom-script-for-customizer' ),
-      'description' => __( 'Add csutom scripts to header or footer', 'custom-script-for-customizer' ),
-  ) );
-
-
-  // Assign sections to panels
-  $wp_customize->get_section('flexia_core_header_script_section')->panel = 'flexia_core_custom_scripts';      
-  $wp_customize->get_section('flexia_core_footer_script_section')->panel = 'flexia_core_custom_scripts';
-
-
+  }
 }
 add_action( 'customize_register', 'flexia_core_customize_register' );
 
