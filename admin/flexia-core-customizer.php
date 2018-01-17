684ed9527bc46b3761c3a1411f94edee83ac247a
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2018-01-17 12:56:45 +0600

    google analytics option added

--- admin/flexia-core-customizer.php
+++ admin/flexia-core-customizer.php
@@ -13,14 +13,14 @@ function flexia_core_customize_register( $wp_customize ) {
   if( class_exists( 'WP_Customize_Code_Editor_Control') ) {
     // Header Script section
     $wp_customize->add_section( 'flexia_core_header_script_section' , array(
-    'title'      => __('Header Script','custom-script-for-customizer'), 
+    'title'      => __('Header Script','flexia-core'), 
     'priority'   => 10    
     ) );  
 
     $wp_customize->add_setting( 'flexia_core_header_script' );
 
       $control = new WP_Customize_Code_Editor_Control( $wp_customize, 'flexia_core_header_script', array(
-              'label'          => __( 'Add your header script without script tag', 'custom-script-for-customizer' ),
+              'label'          => __( 'Add your header script without script tag', 'flexia-core' ),
               'section' => 'flexia_core_header_script_section',
               'code_type' => 'text/javascript',
               'settings'       => 'flexia_core_header_script',
@@ -30,16 +30,32 @@ function flexia_core_customize_register( $wp_customize ) {
     // Footer Script section
 
     $wp_customize->add_section( 'flexia_core_footer_script_section' , array(
-    'title'      => __('Footer Script','custom-script-for-customizer'), 
+    'title'      => __('Footer Script','flexia-core'), 
     'priority'   => 20    
     ) );  
 
     $wp_customize->add_setting( 'flexia_core_footer_script' );
       $control = new WP_Customize_Code_Editor_Control( $wp_customize, 'flexia_core_footer_script', array(
-              'label'          => __( 'Add your footer script without script tag', 'custom-script-for-customizer' ),
+              'label'          => __( 'Add your footer script without script tag', 'flexia-core' ),
               'section' => 'flexia_core_footer_script_section',
               'code_type' => 'text/javascript',
               'settings'       => 'flexia_core_footer_script',
+      ) );
+      $wp_customize->add_control( $control );
+
+    // Google Analytics section
+
+    $wp_customize->add_section( 'flexia_core_ga_script_section' , array(
+    'title'      => __('Google Analytics','flexia-core'), 
+    'priority'   => 30    
+    ) );  
+
+    $wp_customize->add_setting( 'flexia_core_ga_script' );
+      $control = new WP_Customize_Code_Editor_Control( $wp_customize, 'flexia_core_ga_script', array(
+              'label'          => __( 'Paste your Google Analytics code as is.', 'flexia-core' ),
+              'section' => 'flexia_core_ga_script_section',
+              'code_type' => 'text/html',
+              'settings'       => 'flexia_core_ga_script',
       ) );
       $wp_customize->add_control( $control );
 
@@ -47,13 +63,14 @@ function flexia_core_customize_register( $wp_customize ) {
     $wp_customize->add_panel( 'flexia_core_custom_scripts', array(
         'priority' => 999,
         'theme_supports' => '',
-        'title' => __( 'Custom JavaScripts', 'custom-script-for-customizer' ),
-        'description' => __( 'Add csutom scripts to header or footer', 'custom-script-for-customizer' ),
+        'title' => __( 'Custom JavaScripts', 'flexia-core' ),
+        'description' => __( 'Add custom header/footer scripts or Google Analytics', 'flexia-core' ),
     ) );
 
     // Assign sections to panels
     $wp_customize->get_section('flexia_core_header_script_section')->panel = 'flexia_core_custom_scripts';      
     $wp_customize->get_section('flexia_core_footer_script_section')->panel = 'flexia_core_custom_scripts';
+    $wp_customize->get_section('flexia_core_ga_script_section')->panel = 'flexia_core_custom_scripts';
 
   }
 }
--- admin/partials/flexia-core-customizer-admin-display.php
+++ admin/partials/flexia-core-customizer-admin-display.php
@@ -8,14 +8,16 @@
  * @subpackage Flexia_Core/admin
  */
 
+
 // write header script
 
 function flexia_core_add_custom_js_in_wp_head() {
+	$flexia_core_header_script = get_theme_mod('flexia_core_header_script', '');
 
-	if ( ! empty( get_theme_mod('flexia_core_header_script'))) { ?>
+	if ( ! empty( $flexia_core_header_script)) { ?>
 	<script type="text/javascript">
 		/* <![CDATA[ */
-			<?php echo get_theme_mod('flexia_core_header_script'); ?>
+			<?php echo $flexia_core_header_script; ?>
 		/* ]]> */
 	</script>
 	<?php }
@@ -27,8 +29,9 @@ add_action( 'wp_head', 'flexia_core_add_custom_js_in_wp_head' );
 // write footer script
 
 function flexia_core_add_custom_js_in_wp_footer() {
-
-	if (! empty( get_theme_mod('flexia_core_footer_script'))) { ?>
+	$flexia_core_footer_script = get_theme_mod('flexia_core_footer_script', '');
+	
+	if (! empty( $flexia_core_footer_script)) { ?>
 	<script type="text/javascript">
 		/* <![CDATA[ */
 			<?php echo get_theme_mod('flexia_core_footer_script'); ?>
@@ -38,3 +41,15 @@ function flexia_core_add_custom_js_in_wp_footer() {
 
 }
 add_action( 'wp_footer', 'flexia_core_add_custom_js_in_wp_footer' );
+
+// write Google Analytics script
+
+function flexia_core_add_ga_script_in_wp_footer() {
+	$flexia_core_ga_script = get_theme_mod('flexia_core_ga_script', '');
+
+	if (! empty( $flexia_core_ga_script)) {
+		echo $flexia_core_ga_script;
+	}
+
+}
+add_action( 'wp_footer', 'flexia_core_add_ga_script_in_wp_footer' );
